42355ffa5b20e90e7e6010e588414652586af72c
MugaruraEdwin <mugarura13@gmail.com>
2023-10-16 11:45:05 +0300

    Adding comments

--- tests/test.test.js
+++ tests/test.test.js
@@ -6,22 +6,27 @@ const myObj = new Multiply();
 const { assert } = chai;
 
 describe('Multilpy test', () => {
+  // First Test
   it('It should multiply two arguments', () => {
     assert.strictEqual(myObj.multiply(1, 1), 1);
   });
 
+  // Second Test
   it('It should multiply two arguments', () => {
     assert.strictEqual(myObj.multiply(2, 2), 4);
   });
 
+  // Third Test
   it('It should multiply two arguments', () => {
     assert.strictEqual(myObj.multiply(3, 3), 9);
   });
 
+  // Fourth Test
   it('It should multiply two arguments', () => {
     assert.strictEqual(myObj.multiply(4, 4), 16);
   });
 
+  // Fifth Test
   it('It should multiply two arguments', () => {
     assert.strictEqual(myObj.multiply(23, 45), 23 * 45);
   });
