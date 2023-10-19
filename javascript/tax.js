d4a96daaac31cbf02204d8f2775e54364770bdec
MugaruraEdwin <mugarura13@gmail.com>
2023-10-19 08:19:09 +0300

    Tax Calculator final changes

--- javascript/tax.js
+++ javascript/tax.js
@@ -1,3 +1,23 @@
 // If earnings < 12000 per year, pay no tax
 // then with earnings between 12,000 and 36,000 pay 20% tax
 // then with earnings greater than 36000 per year, pay 40% tax
+
+
+class Tax{
+    tax(earnings){
+        if(earnings < 12000){
+            var tax = 0;
+            return tax;
+        }
+        else if(earnings >= 12000 && earnings <= 36000){
+            var tax = earnings * 0.2;
+            return tax;
+        }
+        else if(earnings > 36000){
+            var tax = earnings * 0.4;
+            return tax;
+        }
+    }
+}
+
+module.exports = Tax;
--- test/tax.test.js
+++ test/tax.test.js
@@ -7,7 +7,23 @@ const { assert } = chai;
 
 //test first
 describe("Tax calculator",()=> {
-    it("Taxes paid",()=> {
-        assert.equal(myObj.tax(12000),0)
+    it("Less than 12000",()=> {
+        var salary = 11000;
+        tax = salary * 0;
+        assert.equal(myObj.tax(salary),tax)
     })
+
+    it("Greater than 12000 but less than 36000",()=> {
+        var salary = 35000;
+        tax = salary * 0.2;
+        assert.equal(myObj.tax(salary),tax)
+    })
+
+    it("Greater than 36000",()=> {
+        var salary = 42000;
+        tax = salary * 0.4;
+        assert.equal(myObj.tax(salary),tax)
+    })
+
+
 })
